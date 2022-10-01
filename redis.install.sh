2f680c41724a1d7e11eeb05066e7e22304afb494
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-10-01 23:47:23 +0900

    move to tools

--- redis.install.sh
+++
@@ -1,4 +0,0 @@
-docker pull redis:alpine
-docker network create redis-net
-docker run --name redis -p 6379:6379 --network redis-net -v D:/Projects/Github_new/neis-module/redis/data:/data -d redis:alpine redis-server --appendonly yes
-docker run -it --network redis-net --rm redis:alpine redis-cli -h redis
